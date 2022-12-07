9080454a987440f0621426f417878e623f15c4ab
gEth0 <gEth0.hz@gmail.com>
2022-12-07 10:34:29 +0100

    Notify Timing Changed

--- modules/notify.py
+++ modules/notify.py
@@ -4,7 +4,7 @@ try:
 except:
     print("Make sure you have installed all the dependencies")
 
-def sendNotification(title,message,time=10):
+def sendNotification(title,message,time=5):
     if sys.platform.startswith("win"):
         notification.notify(title=title,message=message,timeout=time,toast=True,app_icon="images\guiLogo.ico")
     else:
